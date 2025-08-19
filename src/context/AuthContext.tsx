a612b4feb33736dbeb42c39ff87da042a9bb3671
jethinthomas-ctrl <jethin.thomas@gmail.com>
2025-08-19 18:55:58 +0530

    updated

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "src/lib/supabaseClient.ts";
+import supabase from "../lib/supabaseClient.ts";
 
 type AuthContextType = {
   user: any;
